3485eba336532fb336cc2215f66bca61b24e7bc8
hhj-data <data981203@gmail.com>
2023-09-22 11:23:44 +0900

    Initial commit

---
+++ src/main/java/com/example/productorederservice/ProductOrederServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.productorederservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductOrederServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductOrederServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/productorederservice/ProductOrederServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productorederservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductOrederServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
